ad88d9a0aa52115ef456d48a5b2e61ee5a116feb
Dat Pham Tien <143339191+datpham0411@users.noreply.github.com>
2024-09-27 11:08:30 +0700

    Update Exercise2.h

--- STM32_LAB1/Core/Inc/Exercise2.h
+++ STM32_LAB1/Core/Inc/Exercise2.h
@@ -8,6 +8,7 @@
 #ifndef INC_EXERCISE2_H_
 #define INC_EXERCISE2_H_
 #include "main.h"
+#include "Exercise1.h"
 void Exercise2_Init();
 void Exercise2_Implement();
 void Set_Color_Pin(GPIO_TypeDef *GPIO_Type, uint16_t Pin_Set, char Set_Color, uint16_t Pin_Unset_1, uint16_t Pin_Unset_2, uint32_t Delay_Time);
